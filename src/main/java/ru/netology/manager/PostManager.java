b93c12f8c223d5eb9ad2b4d7e16a53a8c56a9652
Alexandra Matyukhina <alexandra.paramygina@gmail.com>
2021-06-16 09:15:14 +0300

    added PostManager

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,20 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+
+    private Post[] getPostsForFeed() {
+        return null;
+    }
+
+    public Post[] search(int ownerId, String domain, String query, boolean ownersOnly, int count, int offset, boolean extended) {
+
+        return null;
+    }
+
+
+    public void delete(int ownerId, int postId) {
+    }
+}
